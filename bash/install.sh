34227572edb764bb850ebd94606ae6930d00e1d7
Renan Gonçalves <renan.saddam@gmail.com>
2011-10-25 11:16:55 +0200

    Adding profile and dircolors to non-Mac systems.

--- bash/install.sh
+++ bash/install.sh
@@ -2,6 +2,7 @@
 
 echo 'Installing bash files ...'
 
+# Profile
 if [ -f "$HOME/.bash_profile" ] && [ -f "$HOME/.bash_profile.bak" ]; then
 	echo '- Removing old .bash_profile'
 	rm "$HOME/.bash_profile"
@@ -12,3 +13,18 @@ fi
 
 echo '- Installing new .bash_profile'
 ln -s "$HOME/.dotfiles/bash/profile" "$HOME/.bash_profile"
+
+# Dircolors
+if [ "$(uname -s)" != "Darwin" ]; then
+	if [ -f "$HOME/.dircolors" ] && [ -f "$HOME/.dircolors.bak" ]; then
+        	echo '- Removing old .dircolors'
+	        rm "$HOME/.dircolors"
+	elif [ -f "$HOME/.dircolors" ]; then
+	        echo '- Backing up .dircolors to .dircolors.bak'
+	        mv "$HOME/.dircolors" "$HOME/.dircolors.bak"
+	fi
+
+	echo '- Installing new .dircolors'
+	ln -s "$HOME/.dotfiles/bash/dircolors" "$HOME/.dircolors"
+fi
+
