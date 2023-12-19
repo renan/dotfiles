96054d0f832e39fd5b468c77491fd1d4f4b3ec8f
Renan Gonçalves <renan.saddam@gmail.com>
2023-12-19 11:28:51 -0300

    Remove Sublime 3 stuff

--- SublimeText/install.sh
+++ SublimeText/install.sh
@@ -2,7 +2,7 @@
 
 # See: https://sublime.wbond.net/docs/syncing
 echo 'Installing Sublime Text files ...'
-rm -rf "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-ln -s "$HOME/.dotfiles/SublimeText/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+#rm -rf "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+#ln -s "$HOME/.dotfiles/SublimeText/User" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
